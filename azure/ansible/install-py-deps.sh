ace51d36cc0a70d4f800fbcb0979bcc6b16795c8
Suraj <snetlamu@cisco.com>
2023-10-03 15:01:56 +0530

    Updated and added scripts to fix failures

---
+++ azure/ansible/install-py-deps.sh
@@ -0,0 +1,5 @@
+sudo pip install -t /usr/local/lib/python3.9/dist-packages/ -r /home/developer/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
+sudo pip install azure-cli netaddr
+sudo apt install -y python3-cffi python3-distutils
+sudo pip install pyopenssl --upgrade
+sudo pip install -r /home/developer/.ansible/collections/ansible_collections/azure/azcollection/requirements-azure.txt
